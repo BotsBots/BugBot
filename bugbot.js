8a394f6a89aecf1ca5832c3f429886d5a76ce98c
Ian Mcxa <ianmcxa@tutanota.com>
2016-08-28 15:50:58 -0400

    Added methods to create list and create issues. Moved github integration to module

--- bugbot.js
+++ bugbot.js
@@ -4,9 +4,10 @@
  * Author: Ian Moore
  */
 
-const request = require('request');
 const file = require('fs');
 const yaml = require('js-yaml');
+
+const github = require('./github.js');
 
 /* parses the settings.yml file */
 function readSettings() {
@@ -17,30 +18,13 @@ function readSettings() {
   }
 }
 
-function getRepos(token) {
-  let options = {
-    url: 'https://api.github.com/user/repos',
-    method: 'GET',
-    headers: {
-      'User-Agent': 'EnableIssues',
-      'content-type': 'application/json',
-      'Authorization': 'token ' + token
-    }
-  };
+function main() {
+  let settings = readSettings();
 
-  console.log(options)
-
-  // Send a http request to url and specify a callback that will be called upon its return.
-	request(options, (error, response, body) => {
-    var obj = JSON.parse(body);
-    console.log( obj );
-
-    for( var i = 0; i < obj.length; i++ ) {
-		  var name = obj[i].name;
-		  console.log( name );
-		}
+  github.getIssues(settings.token, 'BotsBots', 'BugBot', (issues) => {
+    console.log(issues);
   });
-}
 
-let settings = readSettings();
-getRepos(settings.token);
+  /*createIssue(settings.token, 'BotsBots', 'BugBot', 'test issue',
+    'this issue created by bugbot through the github API');*/
+}
---
+++ github.js
@@ -0,0 +1,68 @@
+/*
+ * Github API integrations for bugbot
+ *
+ * Author: Ian Moore
+ */
+
+const request = require('request');
+
+/**
+ * Returns information on each of the users repositories
+ */
+exports.getUserRepos = (token) => {
+  let options = {
+    url: 'https://api.github.com/user/repos',
+    method: 'GET',
+    headers: {
+      'User-Agent': 'EnableIssues',
+      'content-type': 'application/json',
+      'Authorization': 'token ' + token
+    }
+  };
+
+  // Send a http request to url and specify a callback that will be called upon its return.
+	request(options, (error, response, body, callback) => {
+     callback(JSON.parse(body));
+  });
+}
+
+/**
+ * List all issues on a given project
+ */
+exports.getIssues = (token, owner, repo, callback) => {
+  let options = {
+    url: 'https://api.github.com/repos/' + owner + '/' + repo + '/issues',
+    method: 'GET',
+    headers: {
+      'User-Agent': 'EnableIssues',
+      'content-type': 'application/json',
+      'Authorization': 'token ' + token
+    }
+  };
+
+  request(options, (error, response, body) => {
+    callback(JSON.parse(body));
+  });
+
+}
+
+/**
+ * Create an issue on a project
+ */
+exports.createIssue = (token, owner, repo, title, body) => {
+  let options = {
+    url: 'https://api.github.com/repos/' + owner + '/' + repo + '/issues',
+    method: 'POST',
+    headers: {
+      'User-Agent': 'EnableIssues',
+      'content-type': 'application/json',
+      'Authorization': 'token ' + token
+    },
+    json: {
+      'title': title,
+      'body': body
+    }
+  };
+
+  request(options);
+}
